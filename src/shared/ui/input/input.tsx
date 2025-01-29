32f737bd789327890a0d5b42db57bdb75c82507a
sfgardev <sf.garol@gmail.com>
2025-01-29 21:47:20 +0700

    fix: make label optional

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,6 +1,6 @@
 import { ComponentPropsWithoutRef, useId } from 'react'
 
-type Props = { label: string } & ComponentPropsWithoutRef<'input'>
+type Props = { label?: string } & ComponentPropsWithoutRef<'input'>
 
 export const Input = ({ label, id: propsId, ...props }: Props) => {
   const generatedId = useId()
@@ -9,9 +9,11 @@ export const Input = ({ label, id: propsId, ...props }: Props) => {
 
   return (
     <div className="flex flex-col gap-1 flex-1">
-      <label htmlFor={id} className="text-base font-medium text-gray-900">
-        {label}
-      </label>
+      {label && (
+        <label htmlFor={id} className="text-base font-medium text-gray-900">
+          {label}
+        </label>
+      )}
       <input
         id={id}
         className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 px-3 py-2"
