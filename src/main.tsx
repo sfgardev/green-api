e09d0de816b75f7a19edd6b68815ce9ae87236d4
sfgardev <sf.garol@gmail.com>
2025-01-29 14:09:40 +0700

    chore: install rtk and connect store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './app/index.css'
+import { Provider } from 'react-redux'
 import App from './app/App'
+import './app/index.css'
+import { store } from './app/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 )
