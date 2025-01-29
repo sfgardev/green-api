f8bf0c0610ab2d698c9e8ec6b0c9fa094ccb73bc
sfgardev <sf.garol@gmail.com>
2025-01-29 16:38:11 +0700

    feat: login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router'
 import { Suspense } from 'react'
+import Login from '@/pages/login'
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path='*' element={<div>Page not found</div>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<div>Page not found</div>} />
       </Routes>
     </Suspense>
   )
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from '@/features/login-form'
+
+const Login = () => {
+  return <div className='h-dvh flex items-center justify-center'>
+    <LoginForm />
+  </div>
+}
+export default Login
