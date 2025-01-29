5a77a1f7241783927f192239543f7951ccad9cc1
sfgardev <sf.garol@gmail.com>
2025-01-29 16:36:01 +0700

    feat: input component

---
+++ src/shared/ui/input/input.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef, useId } from 'react'
+
+type Props = { label: string } & ComponentPropsWithoutRef<'input'>
+
+export const Input = ({ label, id: propsId, ...props }: Props) => {
+  const generatedId = useId()
+
+  const id = propsId ?? generatedId
+
+  return (
+    <div className="flex flex-col gap-1">
+      <label htmlFor={id} className="text-base font-medium text-gray-900">
+        {label}
+      </label>
+      <input
+        id={id}
+        className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 px-3 py-2"
+        {...props}
+      />
+    </div>
+  )
+}
