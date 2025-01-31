ef750d07a8a03b1399380bd3fe9d1252abc653e0
sfgardev <sf.garol@gmail.com>
2025-01-31 18:50:56 +0700

    feat: add home route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router'
 import { Suspense } from 'react'
 import Login from '@/pages/login'
+import Home from '@/pages/home'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<div>Page not found</div>} />
       </Routes>
     </Suspense>
